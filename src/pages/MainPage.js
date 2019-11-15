a1b0f5ab790ccc92eb8c153fe24f8c21c29fb767
Yuriy Chachora <chachora@gmail.com>
2019-11-15 17:43:01 +0400

    Add initial app layout

---
+++ src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Garden from '../components/Garden';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h2>King's Garden</h2>
+      <Garden />
+    </div>
+  );
+};
+
+export default MainPage;
