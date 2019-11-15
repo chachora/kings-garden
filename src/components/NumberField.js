b926e6115769147d6aa4265f279e65afac4c3b04
Yuriy Chachora <chachora@gmail.com>
2019-11-15 19:00:47 +0400

    Add min and max prop to NumberField

--- src/components/NumberField.js
+++ src/components/NumberField.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './NumberField.module.css';
 
-const NumberField = ({ value, label, name, onChange }) => {
+const NumberField = ({ value, label, min, max, name, onChange }) => {
   return (
     <div className={styles.container}>
       <label htmlFor={name}>{label}</label>
       <input
         id={name}
         name={name}
+        min={min}
+        max={max}
         type="number"
         value={value}
         onChange={onChange}
