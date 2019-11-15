f8c977690ab36a39fc9fbaf2780de616108e4381
Yuriy Chachora <chachora@gmail.com>
2019-11-16 01:31:30 +0400

    Add test for draw()

--- src/utils/garden-helper.test.js
+++ src/utils/garden-helper.test.js
@@ -1,9 +1,20 @@
-import { print } from './garden-helper';
+import { print, draw } from './garden-helper';
+import testData from '../../tests/test_data';
 
 describe('print()', () => {
   it('should return string with correct layout', () => {
     const garden = [[2, 1, 1, 0, 1, 1, 2]];
     const expectedValue = '|-- --|';
     expect(print(garden)).toEqual(expectedValue);
+  });
+});
+
+describe('draw()', () => {
+  it('should return correct values for all inputs', () => {
+    testData.forEach(item => {
+      const input = item.input.split(',');
+      const expectedValues = item.pixelArrayJson;
+      expect(JSON.stringify(draw(...input))).toEqual(expectedValues);
+    });
   });
 });
