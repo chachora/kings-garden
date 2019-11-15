c2cc3f6c72833d7310a005056588482974d17f12
Yuriy Chachora <chachora@gmail.com>
2019-11-15 18:34:13 +0400

    Add fields to MainPage

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Garden from '../components/Garden';
+import NumberField from '../components/NumberField';
+import * as gardenHelper from '../utils/garden-helper';
+import styles from './MainPage.module.css';
 
 const MainPage = () => {
+  const [values, setValues] = useState({
+    height: 20,
+    width: 20,
+    padding: 4,
+  });
+
+  const hanldeFieldChange = event => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+
   return (
     <div>
       <h2>King's Garden</h2>
+      <div className={styles.fields}>
+        <NumberField
+          name="width"
+          value={values.width}
+          label={'Width'}
+          onChange={hanldeFieldChange}
+        />
+        <NumberField
+          name="height"
+          value={values.height}
+          label={'Height'}
+          onChange={hanldeFieldChange}
+        />
+        <NumberField
+          name="padding"
+          value={values.padding}
+          label={'Padding'}
+          onChange={hanldeFieldChange}
+        />
+      </div>
       <Garden />
     </div>
   );
