75c74c4dff1b26520c3546bc0092be769e88dadc
chachora <chachora@users.noreply.github.com>
2019-11-16 19:02:54 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.5f61a56060f0cb6916aad87586b07b2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7cd0775d72336940ae8395e8722b4f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ab1128e379413b6f4a6",
+    "url": "./static/css/main.df6aa8ab.chunk.css"
+  },
+  {
+    "revision": "5974e14d556507ff737a",
+    "url": "./static/js/2.cf46b9a4.chunk.js"
+  },
+  {
+    "revision": "0ab1128e379413b6f4a6",
+    "url": "./static/js/main.8632fbdb.chunk.js"
+  },
+  {
+    "revision": "5d9598adc49e55765f68",
+    "url": "./static/js/runtime-main.e4694fa2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9657d6956735622fdeccd132645c1796.js"
+  "./precache-manifest.5f61a56060f0cb6916aad87586b07b2f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
